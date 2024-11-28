6bbc4b92b592d33f61b17bfb294c0adc6aa0fba7
Eray <eray.cevik@digitecgalaxus.ch>
2024-11-28 11:47:15 +0100

    Created Client

--- Desktop/Privat/Designs/ForDevs/Dashboard-Full/Dashboard-Full/Program.cs
+++ Desktop/Privat/Designs/ForDevs/Dashboard-Full/Dashboard-Full/Program.cs
@@ -1,18 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudWind.Website;
-using MudBlazor.Services;
-using MudWind.Services;
+using Dashboard_Full;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
-builder.Services.AddThemeServices(new ThemeManagerConfiguration
-{
-     Theme = Resources.TailFramesInspirationTheme,
-     LocalStorageKey = Resources.LocalStorageKey,
-     IsDarkMode = true
-});
+
 await builder.Build().RunAsync();
